4f3887a5eb1ee5dd5eb18f011bbde14f8e59df85
Simon Heather <32168619+X-Guardian@users.noreply.github.com>
2023-08-05 21:41:54 +0100

    chore: Fix Go Static Check Errors (#3637)
    * Fix Go Static Checks
    
    * Fix working dir tests

--- server/events/project_command_builder.go
+++ server/events/project_command_builder.go
@@ -769,7 +769,7 @@ func (p *DefaultProjectCommandBuilder) buildProjectCommandCtx(ctx *command.Conte
 		if repoCfgPtr.ParallelPlan != nil {
 			parallelPlan = *repoCfgPtr.ParallelPlan
 		}
-		abortOnExcecutionOrderFail = *&repoCfgPtr.AbortOnExcecutionOrderFail
+		abortOnExcecutionOrderFail = repoCfgPtr.AbortOnExcecutionOrderFail
 	}
 
 	if len(matchingProjects) > 0 {
