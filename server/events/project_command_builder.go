da7dad0f3e7d37fb237d1a47dedb64b02429b77e
Gabriel Martinez <19713226+GMartinez-Sisti@users.noreply.github.com>
2023-12-11 19:02:16 +0000

    chore(atlantis): fix linter errors (#3690)
    * chore(atlantis): fix linter errors
    
    * fix superfluous-else
    
    * revert gitlab check
    
    * ignore stub funcs on azuredevops_client
    
    * remove fetch-depth
    
    * remove fail_on_error
    
    * fix plan_step_runner_test.go
    
    * more lint fixes
    
    ---------
    
    Co-authored-by: Dylan Page <dylan.page@autodesk.com>

--- server/events/project_command_builder.go
+++ server/events/project_command_builder.go
@@ -120,7 +120,7 @@ func NewProjectCommandBuilder(
 	IncludeGitUntrackedFiles bool,
 	AutoDiscoverMode string,
 	scope tally.Scope,
-	logger logging.SimpleLogging,
+	_ logging.SimpleLogging,
 	terraformClient terraform.Client,
 ) *DefaultProjectCommandBuilder {
 	return &DefaultProjectCommandBuilder{
